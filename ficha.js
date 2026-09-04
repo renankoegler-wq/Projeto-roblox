4f08658ebe0f255ff447f2d37572544ab9cdfa2d
NICOLAS CERVANTES FIEDLER <nicolas_fiedler@estudante.sesisenai.org.br>
2026-09-03 23:11:54 -0300

    Ficha

---
+++ ficha.js
@@ -0,0 +1,10 @@
+let nomeItem = ('Espada das sombras');
+let precoItem = (250);
+let raridadeItem = ('Raro');
+let estoque = (12);
+
+console.log('=== FICHA DO ITEM === + \n');
+console.log('Nome: ' + nomeItem + '\n');
+console.log('Preço: R$ ' + precoItem + '\n');
+console.log('Raridade: ' + raridadeItem + '\n');
+console.log('Estoque: ' + estoque + '\n');
