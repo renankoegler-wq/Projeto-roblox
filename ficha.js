b9d73303584870c998ca3ab6ba4339516bdc98f4
Renan Bento Koegler <renan_koegler@estudante.sesisenai.org.br>
2026-09-09 22:23:37 -0300

    final

--- ficha.js
+++ ficha.js
@@ -58,37 +58,44 @@ console.log(`Raridade: ${raridadeItem[i]}  \n`);
 
 console.log(`Estoque: ${estoque[i]} \n`);
 if (disponivel[i] == true) {
-    console.log('Disponível')
+    console.log('Disponível');
 }
 else {
-    console.log('Indisponível \n\n')
+    console.log('Indisponível \n\n');
 }
 if (i % 2 - 1 === 0) {
     
-        console.log('Item em SUPER promoção da semana!\n\n')
+        console.log('Item em SUPER promoção da semana!\n\n');
 }
 
 
 
     
-console.log(`Item ${i+1} cadastrado`)
+console.log(`Item ${i+1} cadastrado`);
 }
 
+resposta = prompt('deseja testar uma venda? ');
 
+if (resposta == 'sim') {
+    resposta = prompt('qual número do cadastro do produto? ');
+    resposta = resposta-1
+    console.log('testando vendas.');
+    console.log(`Estoque: ${estoque[resposta]} atual \n`);
+    while (estoque[resposta] > 0)
+    {
+        console.log(`estoque atual: ${estoque[resposta]} `)
+        estoque[resposta] = estoque[resposta]-2
+    }
 
+    console.log('estoque acabado!')
+}
 
+resposta = prompt('você deseja utilizar o catálogo? ')
+if (resposta == 'sim') {
+    for (let itemAtual of nomeItem) {
+        console.log (`item atual: ${itemAtual}`)
+        prompt('aperte enter para ir para o proximo item')
+    }        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log('você chegou ao fim do catálogo!')
+}
